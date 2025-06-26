465479a4285dfda34c5f55414782a65be906750c
Leonardo <leoccapra@gmail.com>
2025-06-25 21:29:17 -0300

    feat leo: Criando o projeto, primeira aula do projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JAVA-POO-NTT-DATA
 
+teste
---
+++ app/src/main/java/br/com/dio/model/InvestmentWallet.java
@@ -0,0 +1,32 @@
+package br.com.dio.model;
+
+import static br.com.dio.model.BankService.INVESTMENT;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+
+public class InvestmentWallet extends Wallet {
+    private final Investment investment;
+    private final AccountWallet account;
+
+    public InvestmentWallet(final Investment investment, final AccountWallet account, final long amount) {
+        super(INVESTMENT);
+        this.investment = investment;
+        this.account = account;
+        addMoney(account.reduceMoney(amount), getService(), "investimento");
+    }
+
+    public void updateAmount(final long percent) {
+        var amount = getFunds() * percent / 100;
+        var history = new MoneyAudit(UUID.randomUUID(), getService(), "rendimento", OffsetDateTime.now());
+        var money = Stream.generate(() -> new Money(history)).limit(amount).toList();
+        this.money.addAll(money);
+    }
+}
